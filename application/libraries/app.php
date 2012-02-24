0f37dc344d5d9d048fabcfd61ad749fb962321df
Colin Viebrock <colin@viebrock.ca>
2012-02-24 13:19:39 -0600

    Pull random words from DB now

--- application/libraries/app.php
+++ application/libraries/app.php
@@ -62,65 +62,20 @@ class App {
 
 	public static function rwords($num=3, $with_score=true) {
 
-		$words = array(
-			'secretly','blithers','rigorism','updarted','airproof','misspelt','mesotron','stonable','pronging','rebutted',
-			'premolds','vibrioid','pupilary','knollers','planular','trappose','sleekier','hyperope','urodeles','misunion',
-			'liberate','corrades','entryway','literacy','intombed','enuresis','varietal','prefects','thrivers','feastful',
-			'misdoing','advisory','erogenic','meltages','groupoid','pitfalls','bafflers','chromize','pureeing','cordwood',
-			'tentlike','drizzled','submenus','unhanged','trochili','rezeroes','tweenies','folkways','legacies','palterer',
-			'shealing','retracer','suffered','autocrat','employee','wagtails','absorber','combings','tepefied','cubistic',
-			'grieving','morelles','ammoniac','sculptor','ungently','sealifts','awlworts','airbuses','bargeman','parsleys',
-			'neuromas','assented','currency','statuses','begotten','vainness','coupling','wheezier','sonobuoy','fistnote',
-			'samovars','ectozoon','mirepoix','username','reinjury','fascicle','unwitted','unyoking','homelike','slowdown',
-			'frenches','trolands','applause','martlets','trickled','economic','rockiest','pennames','question','cheesing',
-			'commoner','greenery','sherbets','bestowal','santeras','teethers','tickings','womaning','molybdic','mungoose',
-			'braunite','riftless','updating','invocate','toddling','kindling','grayness','cullions','anchusas','debagged',
-			'frothier','dragomen','prickled','calpains','knottier','crooning','shrieved','enisling','overurge','hounders',
-			'madronas','hackable','crudding','unpurged','purblind','ejective','crankily','reactant','evermore','thorites',
-			'overpays','sawteeth','sinciput','neurular','politics','resaddle','faddists','typifier','chelator','hypogene',
-			'misspace','greasers','idealize','listable','flappier','sithence','inputted','temperas','cauldron','nervures',
-			'succubas','pretrial','gadflies','scenario','doddered','amphipod','xeroxing','satiated','minutiae','capsuled',
-			'sinusoid','ungraded','bullpout','printing','valuates','streeker','saboteur','hematine','complins','moseying',
-			'garaging','sandfish','soochong','hyacinth','romaunts','melanist','unframed','creepage','homogony','squarish',
-			'fairlead','seemings','sprinted','feminise','appellor','hoydened','tutoyers','resorter','polypeds','ringdove',
-			'nonblack','epoxides','acrotism','orreries','barrator','foulness','ovoidals','letdowns','publican','fuckoffs',
-			'smaltine','stampers','degrader','teratism','unwaning','farcical','overhard','tallisim','cabbalah','respaces',
-			'sordidly','gleeking','aplastic','upswings','dianthus','oogamies','ratifier','quackish','burblers','hotelier',
-			'analemma','truthful','capelins','guesting','hatchway','sheathed','solarism','teniases','invading','halluces',
-			'bevelled','organums','antedate','talkiest','ribworts','shunpike','shrewdie','buxomest','quetzals','cipolins',
-			'pastness','squatted','outburst','mucidity','believer','subagent','cabotage','butyrate','pyrolyze','preppily',
-			'shoptalk','weirdoes','inclines','reassume','isopodan','noisiest','sectored','outpours','performs','cultches',
-			'pilosity','outhauls','moveless','vegetate','fervency','popelike','unnerves','slickest','smooched','squeaker',
-			'trifling','dawdling','frayings','meditate','lanciers','tornados','aglimmer','unrooted','dottiest','spookish',
-			'hotheads','fastball','praisers','sinfonie','ingrates','swizzles','mendable','cadaster','handlike','enwheels',
-			'paranoea','remanent','fameless','isolines','relearnt','zorillas','crankpin','arrogate','benomyls','pilsener',
-			'faithing','bistoury','outmodes','throning','rindless','eversion','adulates','foregone','inflated','desulfur',
-			'epitases','underfed','bubaline','exsecant','sodomist','shortias','baptisms','flutters','symphony','infarcts',
-			'maintain','shippens','rarefied','flagrant','brassily','cambiums','oversoon','tangling','defunded','benefice',
-			'chigetai','bedights','beclasps','waggling','dysurias','voidness','strawier','drowners','twirlier','tankards',
-			'nattiest','firebomb','caressed','spumones','roseroot','extrados','swankier','mistypes','amphorae','paganize',
-			'milepost','hockshop','forkedly','nudeness','fatbirds','livening','limberer','progress','milesimo','proofing',
-			'tearless','confined','subtlety','hematoid','quintets','ashcakes','offshoot','turquois','blastula','lysogens',
-			'moshavim','thundery','chording','octuplet','danegelt','anthemed','rattoons','subtheme','lynching','nonelite',
-			'quantile','coumarou','shudders','cutovers','impolite','mercuric','firewood','aluminum','vicenary','amoretti',
-			'truffles','oilproof','partitas','apostils','secretin','macrames','flapping','killjoys','imbrutes','porkwood',
-			'joggling','regilded','chuggers','convenor','nostrils','malemute','foresaid','detacher','uncrated','drencher',
-			'vanishes','jeremiad','diazepam','anaphase','extended','supertax','soarings','asterias','warrigal','shirkers',
-			'pisiform','libretto','clevises','virtuose','shavings','wettings','sashless','pharmacy','writhing','dandyism',
-			'bespoken','stenoses','cabalism','downlike','splendor','bowfront','workshop','unionism','ocarinas','chilling',
-			'absolves','mongered','coholder','mainline','waterlog','aduncate','inkwoods','sartorii','shucking','sergeant',
-			'promiser','doweling','unionise','cookable','matzoons','dizziest','ceramals','broadest','carcanet','contests',
-			'staggart','pudibund','editable','okeydoke','forehead','shiplaps','roomiest','slithers','thorough','defecate',
-			'conchies','beholden','funnyman','speeders','hebetude','perspiry','presoaks','sweetens','pantalet','pickoffs',
-			'marauder','retinula','resculpt','chestful','saltings','uncrates','divorces','plaguers','goatskin','guaranty'
+		$words = DB::query('SELECT
+			word
+			FROM validwords
+			ORDER BY RAND()
+			LIMIT ?',
+			array($num)
 		);
 
 		$return = '';
+		foreach($words as $word) {
 
-		while($num--) {
 			if ($return) $return .= ', ';
 
-			$return .= $words[ mt_rand(0,count($words)-1) ];
+			$return .= $word->word;
 
 			if ($with_score) {
 				$return .= ' ' . mt_rand(60,100);
