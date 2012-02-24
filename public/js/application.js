ef5e4a1e48c70bd3598b570a46c60074579d8e13
Colin Viebrock <colin@viebrock.ca>
2012-02-24 12:36:01 -0600

    tweaks

--- public/js/application.js
+++ public/js/application.js
@@ -1,1 +1,40 @@
-/* nothing */
+
+// array.has() prototype
+
+Array.prototype.has=function(v) {
+	for (i=0;i<this.length;i++) {
+		if (this[i]==v) return i;
+	}
+	return undefined;
+}
+
+
+// our own sorting methods for tablesorter
+
+$.tablesorter.addParser({
+	id: 'sc_date',
+	is: function(s) { return false; },
+	format: function(s) {
+		return new Date(s).getTime();
+	},
+	type: 'numeric'
+});
+
+
+$.tablesorter.addParser({
+	id: 'sc_record',
+	is: function(s) { return false; },
+	format: function(s) {
+		console.log(s);
+		var p = s.split(' ');
+		if (p.length==3) {
+			var n = parseFloat(p[0]);
+			var d = parseFloat(p[2]);
+			if (n+d == 0)  return -1;
+			return n/(n+d);
+		}
+		return -1;
+	},
+	type: 'numeric'
+});
+
