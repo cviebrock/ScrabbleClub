6d4fdd7a30a3f2e2ac39751a38d95865d429cd63
Colin Viebrock <colin@viebrock.ca>
2012-02-24 12:35:42 -0600

    One on one history

--- application/routes/ajax.php
+++ application/routes/ajax.php
@@ -14,3 +14,34 @@ Route::get('ajax/players', array( 'as'=>'ajax_players', 'before' => 'ajax', func
 	return json_encode($data);
 
 }));
+
+Route::get('ajax/games/(:num)/vs/(:num?)', array( 'as'=>'ajax_one_on_one', 'before' => 'ajax', function($player_id,$opponent_id=null)
+{
+
+// the second arg isn't really optional, but if it isn't specified this way,
+// then you can't use URL::to_route() without giving both args ... and
+// we use jQuery to generate that second arg.
+
+	usleep(500000);
+
+	$games = Game::where('player_id','=',$player_id)
+		->where('opponent_id','=',$opponent_id)
+		->order_by('date','desc')
+		->get();
+
+	if (count($games)) {
+
+		echo View::make('partials.game_listing')
+			->with('games', $games)
+			->with('id', 'ooo_games')
+			->with('mark_winners', true)
+			->with('small_head', true)
+			->render();
+
+	} else {
+
+		echo '<p class="ooo_no_history">No matching games.</p>';
+
+	}
+
+}));
