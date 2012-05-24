61a2628c64e62749ce5512c3b110c99c2da0e88d
Colin Viebrock <colin@viebrock.ca>
2012-05-24 16:42:40 -0500

    adjust span sizes

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -1,11 +1,11 @@
 <footer>
 	<div class="navbar-inner">
 		<div class="container-fluid">
-			<div class="span6">
-				SCRABBLE&reg; is a registered trademark of HASBRO, Inc., manufacturer of the SCRABBLE&reg; Brand Crossword Game.
+			<div class="span8 disclaimer">
+				SCRABBLE&reg; is a registered trademark of HASBRO, Inc., manufacturer of the SCRABBLE&reg; Brand Crossword Game.<br>
 				The SCRABBLE&reg; trademark is owned by J.W. Spear and Sons, PLC, a subsidiary of Mattel, Inc. outside of the U.S. and Canada.
 			</div>
-			<div class="span6 pull-right">
+			<div class="span4 copyright pull-right">
 				<a href="http://github.com/cviebrock/ScrabbleClub" target="_blank">Scrabble Club Portal Software</a><br>
   			&copy; 2012 Colin Viebrock
   		</div>
