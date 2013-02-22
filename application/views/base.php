d277fa4e681088b7e770da63d4e0808a2a182dc5
Colin Viebrock <colin@viebrock.ca>
2013-02-22 09:46:43 -0600

    add FB code

--- application/views/base.php
+++ application/views/base.php
@@ -8,6 +8,16 @@
 	<?php echo Asset::container('head')->scripts(); ?>
 </head>
 <body>
+<?php if ($appid = Config::get('facebook.appId')): ?>
+	<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=<?php echo $appid; ?>";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<?php endif; ?>
 	<?php echo $header; ?>
 	<div class="container">
 	<?php echo $flashes; ?>
