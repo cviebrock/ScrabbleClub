de44eb50599c6c85827ff133ca52d579dd64586d
Colin Viebrock <colin@viebrock.ca>
2013-02-22 09:46:19 -0600

    fb config

---
+++ application/config/facebook.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+
+	// you can put the values directly in here
+	// this is just to keep them secret from github
+
+
+	'appId'      => ini_get('scrabble.facebook.appid'),
+	'secret'     => ini_get('scrabble.facebook.secret'),
+	'fileUpload' => false,
+	'uid'        => ini_get('scrabble.facebook.uid'),
+
+);
