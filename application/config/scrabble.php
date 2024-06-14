b79a4be25ac4c2e8d2c5df9e922f29284a233f1f
Colin Viebrock <colin@viebrock.ca>
2024-06-13 22:10:53 -0400

    change wordlist

--- application/config/scrabble.php
+++ application/config/scrabble.php
@@ -4,7 +4,7 @@ return array(
 	'clubname'     => 'Winnipeg Scrabble Club',
 	'first_year'   => 2012,
 	'current_year' => date('Y'),
-	'current_wordlist' => 'nwl2020',
+	'current_wordlist' => 'wow24',
 
 	'initial_rating' => 800
 );
